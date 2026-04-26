8e3265608c108d2a21067695d82ac6d714853fec
miced <miced@mailbox.org>
2026-04-26 14:31:34 +0200

    test secret

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,12 @@ package org.example;
 public class Main {
     static void main() {
         System.out.println("Hello World from Java!");
+
+        String dbUser = System.getenv("DB_USER");
+        if (dbUser == null) {
+            System.out.println("DB_USER not set");
+        } else {
+            System.out.println("DB_USER starts with: " + dbUser.substring(0, Math.min(2, dbUser.length())) + "***");
+        }
     }
 }
